569a34aa3edd7221e1877438cb2fd9abe988cbd4
doyousketch2 <doyousketch2@yahoo.com>
2017-07-05 05:43:40 -0400

    Create enemy.lua

---
+++ libs/enemy.lua
@@ -0,0 +1,45 @@
+enemy  = {}  -- namespace
+meta  = {  __index  = enemy  } -- metatable.
+
+-- prototype / default values can be included here,  but they are generated once.
+-- if you want unique values for each instance,  generate them when creating clone.
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function enemy :dia() -- create an object
+  local instance  = {  name  = 'diamond',
+                   segments  = 4,
+                          R  = mat .random( 0,  100 ),
+                          G  = mat .random( 50,  255 ),
+                          B  = mat .random( 0,  50 ),
+                       size  = mat .random( 5,  15 ),
+                          x  = mat .random( 10,  WW -10 ),
+                          y  = mat .random( 10,  HH -10 ),
+                         vx  = mat .random( -50,  50 ),
+                         vy  = mat .random( -50,  50 )  }
+
+  setmetatable( instance,  meta ) -- link instance with metatable
+  return instance
+end -- tri
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function enemy :hex() -- generate new local colors and sizes
+  local instance  = {  name  = 'hexagon',
+                   segments  = 6,
+                          R  = mat .random( 50,  255 ),
+                          G  = mat .random( 0,  100 ),
+                          B  = mat .random( 0,  50 ),
+                       size  = mat .random( 5,  15 ),
+                          x  = mat .random( 10,  WW -10 ),
+                          y  = mat .random( 10,  HH -10 ),
+                         vx  = mat .random( -50,  50 ),
+                         vy  = mat .random( -50,  50 )  }
+
+  setmetatable( instance,  meta )
+  return instance
+end -- squ
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+return enemy
