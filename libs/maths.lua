8df69ab733097b7aacb894d7fbbf92cca16bc2d8
doyousketch2 <doyousketch2@yahoo.com>
2018-11-06 03:07:32 -0500

    Update maths.lua

--- libs/maths.lua
+++ libs/maths.lua
@@ -1,4 +1,8 @@
+
+--  love2d.org/wiki/General_math
+--  codeplea.com/simple-interpolation
 --  lua-users.org/wiki/MathLibraryTutorial
+--  paulbourke.net/miscellaneous/interpolation
 --  www.tutorialspoint.com/lua/lua_standard_libraries_math_library.htm
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -105,10 +109,12 @@ exp  = math.exp
 --  exp( 1 )    ;   = 2.718281828459  = e
 --  exp( 27 )   ;   = 532048240601.8
 
-log  = math.log  --   inverse of math.exp
+log  = math.log -- (x [, base])   inverse of math.exp,  optional base
 --  log( 532048240601 )   ;   = 26.999999999998
---  log( 3 )              ;   = 1.0986122886681
---  log( -3 )             ;   = nan
+--  log( 2 )              ;   = 0.69314718055995
+--  log( 2, 2 )           ;   = 1
+--  log( -2 )             ;   = nan
+--  log( 1000, 10 )       ;   = 3
 
 log10  = math.log10  --  base-10 logs
 --  log10( 1000 )    ;   = 3
@@ -117,45 +123,47 @@ log10  = math.log10  --  base-10 logs
 
 --  returns x^y
 pow  = math.pow
---  pow( 2, 10 )   ;   1024
---  2 ^ 10         ;   1024
+--  pow( 2, 10 )   ;   = 1024
+--  2 ^ 10         ;   = 1024
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 --  manipulate floats without direct bit manipulation
---  Returns normalized fraction (mantissa) & exponent.  x = m2e
-frexp  = math.frexp
---  frexp( 5 )   ;   0.625   3
---  frexp( 9 )   ;   0.5625   4
---  frexp( 1024 )   ;   0.5   11
---  e  = int  and  abs(m) is range(  0.5(inclusive),  1(exclusive)  )
---                        ...or zero if x = zero
+--  Returns normalized fraction (mantissa) & integer exponent.
+frexp  = math.frexp -- (x)  = m2e
+--  frexp( 5 )   ;   = 0.625   3
+--  frexp( 9 )   ;   = 0.5625   4
+--  frexp( 1024 )   ;   = 0.5   11
+--  abs(m) is range(  0.5(inclusive),  1(exclusive)  )
+--                 ...or zero if x = zero
 
---  Returns m2e (e = integer)
+--  m2e (e = integer)
 ldexp  = math.ldexp
---  ldexp( 1, 9 )       ;   512
---  ldexp( 1, 10 )       ;   1024
---  ldexp( 1000000, 1 )   ;   2000000
+--  ldexp( 1, 9 )       ;   = 512
+--  ldexp( 1, 10 )       ;   = 1024
+--  ldexp( 1000000, 1 )   ;   = 2000000
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 --  minimum or maximum value from variable length list of args.
-min  = math.min
+min  = math.min --  (x, ···)
 --  min( 1,  2 )          ;   = 1
 --  min( 1.2,  7,  3 )    ;   = 1.2
 --  min( 1.2,  -7,  3 )   ;   = -7
-max  = math.max
+max  = math.max -- (x, ···)
 --  max( 1.2,  -7,  3 )   ;   = 3
 --  max( 1.2,  7,  3 )    ;   = 7
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 --  floating-point remainder of x / y.  rounds towards zero
-mod,  fmod  = math.fmod,  math.fmod
+fmod  = math.fmod -- (x, y)
+
 --  fmod( 9.5,  -3.5 )   ;   -2.5
 --  fmod( -9.5,  3.5 )   ;   2.5
 --  fmod( 9.5,  0 )      ;   -nan
 
+--  mod  = math.mod  -- deprecated in Lua, don't use
 --  for modulus (remainder) use modulo % operator instead.  rounds to -inf
 --  9.5 % -3.5   ;   1
 --  -9.5 % 3.5   ;   -1
@@ -177,46 +185,44 @@ sqrt  = math.sqrt
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
---  generate pseudo-random numbers uniformly distributed
-random  = math.random
+--  generate pseudo-random numbers, uniformly distributed
+random  = math.random -- ([m [, n]])
 --  random() no args = real number between 0 and 1.
 --  random( upper )  = integers between 1 & upper (both inclusive).
 --  random( lower, upper ) ints between lower & upper (inclusive)
 
---  random()   ;   = 0.0012512588885159
---  random()   ;   = 0.56358531449324
---  random( 100 )   ;   = 20
---  random( 100 )   ;   = 81
+--  random()     ;   = 0.56358531449324
+--  random( 1 )   ;   = 1  no room between 1 and 1,  will always return 1
+--  random( 2 )     ;   = 1  use 2 instead
+--  random( 2 )      ;   = 2
+--  random( 100 )     ;   = 81
 --  random( 70, 80 )   ;   = 76
---  random( 70, 80 )   ;   = 75
---  upper and lower MUST be integer, else unexpected results
-
+--  upper and lower MUST be positive ints, else crash
 
 --  seed for pseudo-random gen:  Equal seeds produce equal sequences
-randomseed  = math.randomseed
---  randomseed( 1234 );  random(), random(), random()
---  0.12414929654836        0.0065004425183874      0.3894466994232
---  randomseed( 1234 );  random(), random(), random()
---  0.12414929654836        0.0065004425183874      0.3894466994232
+--  mat .setRandomSeed( 1234 );  random(), random(), random()
+--  0.12414929654836    0.0065004425183874    0.3894466994232
+--  mat .setRandomSeed( 1234 );  random(), random(), random()
+--  0.12414929654836    0.0065004425183874    0.3894466994232
 
---  good* 'seed' is os.clock(),  wait a second before calling again.
---  first number not really 'random' (Win 2K & OS X)
+--  good seed is os.clock(),  wait a second before calling again.
+--  first number not really 'random' (Win2K & OSX)
 --  roll the dice a couple times to prime them.
 
---  randomseed( os.clock() );  random(), random(), random()
+--  mat .setRandomSeed( os.clock() );  random(), random(), random()
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 --  represents +infinity. 
 huge  = math.huge
---  huge       ;   = inf
---  huge / 2    ;   = inf
+--  huge      ;   = inf
+--  huge /2    ;   = inf
 --  -huge       ;   = -inf
---  huge/huge    ;   = nan
---  huge * 0     ;   = nan
---  1/0           ;   = inf
---  1/0 == huge   ;   = true
---  huge == huge   ;   = true
+--  huge /huge   ;   = nan
+--  huge *0       ;   = nan
+--  1/0            ;   = inf
+--  1/0 == huge     ;   = true
+--  huge == huge     ;   = true
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -229,10 +235,168 @@ tau        = pi *2
 
 --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
---[[
-math.maxinteger
-math.mininteger
-math.tointeger
-math.type
-math.ult
-]]
+function clamp( number, low, high )
+  low  = low or 0
+  high  = high or 1
+  return min( max( low, number ), high )
+end
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--  Interpolation methods
+
+-- jump from one value to the next halfway through
+function step( a, b,  mu )
+  if mu < 0.5 then  return a
+  else  return b
+  end
+end
+
+
+-- straight Linear interpolation between two numbers.
+function lerp( a, b,  mu )
+  return a +(b -a) *mu
+end
+
+
+-- curvy Cosine interpolation between two numbers.
+function cerp( a, b,  mu )
+  local f  = (1 -cos(mu *pi)) *0.5
+  return a *(1 -f) +b *f
+end
+
+
+--  similar to cerp, but faster
+function smoothStep( a, b,  mu )
+  return lerp( a,  b,  mu ^2 *(3 -2 *mu) )
+end
+
+
+--  quad-in speed up
+function acceleration( a, b,  mu )
+  return lerp( a,  b,  mu ^2 )
+end
+
+
+-- quad-out slow down
+function deceleration( a, b,  mu )
+  return lerp( a,  b,  1- (1 -mu) ^2 )
+end
+
+
+--  gently ramp up, then down
+function quad_inout( a, b,  mu )
+  if t < 0.5 then  return acceleration( a, b,  mu )
+  else  return deceleration( a, b,  mu )
+  end
+end
+
+
+-- soft cubic interpolation between 2 endpoints, + 2 outer control points
+function cubic( y1, y2, y3, y4,  mu )
+  local mu2  = mu ^2
+  local a1  = y4 -y3 -y1 -y2
+  local a2  = y1 -y2 -a1
+  local a3  = y3 -y1
+  local a4  = y2
+
+  return ( a1 *mu *mu2 +a2 *mu2 +a3 *mu +a4 )
+end
+
+
+--  Tension: -1 is low,  0 normal,  1 high
+--  Bias: neg towards last segment,  0 is even,  pos to first segment
+function hermite( y1, y2, y3, y4, mu, tension, bias )
+  local mu2  = mu ^2
+  local mu3  = mu2 *mu
+
+  local m1  =     (y2 -y1) *(1 +bias) *(1 -tension) /2
+  local m1  = m1 +(y3 -y2) *(1 -bias) *(1 -tension) /2
+  local m2  =     (y3 -y2) *(1 +bias) *(1 -tension) /2
+  local m2  = m2 +(y4 -y3) *(1 -bias) *(1 -tension) /2
+
+  a1 =  2 *mu3 -3 *mu2 +1
+  a2 =     mu3 -2 *mu2 +mu
+  a3 =     mu3 -   mu2
+  a4 = -2 *mu3 +3 *mu2
+
+  return ( a1 *y2 +a2 *m1 +a3 *m2 +a4 *y3 )
+end
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function angle( x1, y1,  x2, y2 ) -- angle between two points.
+  return atan2( y2 -y1,  x2 -x1 )
+end
+
+
+function dist( x1, y1,  x2, y2 )  --  distance between two points.
+--  Pythag       aSquared + bSquared.   squareRoot = c
+  return sqrt((x2 -x1) ^2 + (y2 -y1) ^2)
+end
+
+
+function dist3d( x1, y1, z1,  x2, y2, z2 ) -- between two 3D points.
+--  Pythag      aSquared  +  bSquared  +  cSquared.   squareRoot = d
+  return sqrt((x2 -x1) ^2 + (y2 -y1) ^2 + (z2 -z1) ^2)
+end
+
+
+--  Normalize two numbers, common scale
+function normalize( x, y )
+  --  Pythag      aSqu + bSqu.  SquRt = c
+  local c  = sqrt(x ^2 + y ^2)
+
+  if c == 0 then  return 0,0,0
+  else  return x /c,  y /c,  c -- ratios with a common denomonator
+  end
+end -- normalize
+
+--  example:
+--  a, b, c  = normalize( 8, 13 )
+--  print( a *c,  b *c )   ;   8  13
+
+
+--  1 if positive,  -1 if negative,  or 0
+function sign( number )
+  if number > 0 then  return 1
+  elseif number < 0 then  return -1
+  else  return 0
+  end
+end -- sign
+
+--  Randomly returns either -1 or 1
+function rsign()
+  if random() < 0.5 then return -1
+  else return 1
+  end
+end
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+--[[  Lua 5.3 notes.
+  not compatible w/ LuaJIT, so not likely to be in Love2D,
+  but for those learning Lua, it's nice to know the differences
+
+--  optional x, more like atan2.  default value for x is 1
+math.atan (y [, x])
+
+--  integer with minimum value for an integer
+math.mininteger   ;   = -9223372036854775808
+
+--  integer with maximum value for an integer
+math.maxinteger   ;   = 9223372036854775807
+
+--  if x is convertible to int, returns int. Otherwise, nil
+math.tointeger( 1 )    ;   = 1
+math.tointeger( 1.1 )  ;   = nil
+
+--  Returns "integer",  "float",  or "nil" if x is not a number
+math.type( 3 )    ;   = integer
+math.type( 3.3 )  ;   = float
+math.type( 'a' )  ;   = nil
+
+--  true if int m is below int n
+math.ult( 2, 3 )   ;   = true
+math.ult( 3, 2 )   ;   = false
+
+]]--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
